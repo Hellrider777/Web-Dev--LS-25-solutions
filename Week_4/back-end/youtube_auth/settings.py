70aa66d5d2d0fa9f187cf06eca1310152982cf3f
Aditya Adhana <adityaadhana0409@gmail.com>
2026-01-30 20:56:49 +0530

    chore: Update database config from SQLite3 to PostgreSQL with environment variables
    - Changed DATABASES config to use PostgreSQL backend
    - Database credentials now read from environment variables
    - Supports standard DevOps deployment practices
    - Aligns with docker-compose.prod.yml and deployment configurations

--- Week_4/back-end/youtube_auth/settings.py
+++ Week_4/back-end/youtube_auth/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'youtube_auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'ytclone_adhana'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
